48987afe415595d0557b8f68cd36bbea3fbe4905
smarinic <34188840+smarinic@users.noreply.github.com>
2022-03-15 18:32:05 +0100

    [fix] alert session variable typo fixed

--- php/alert.message.handler.php
+++ php/alert.message.handler.php
@@ -1,17 +1,30 @@
 <?php
+
+function createAlertMessage($type, $message) {
+  $_SESSION['alert_type'] = $type;
+  $_SESSION['alert_message'] = $message;
+}
+
 function showAlertMessage() {
-  $message = '';
-  if(isset($_SESSION['redirect_message']) && $_SESSION['redirect_message'] != '') {
-    if($_SESSION['redirect_type'] == 'success') {
-      $message = '<div class="alert alert-success" role="alert">' . $_SESSION['redirect_message'] . '</div>';
+  if(isset($_SESSION['alert_type'], $_SESSION['alert_message']))  {
+    if($_SESSION['alert_type'] == 'success') {
+      $message = '<div class="alert alert-success" role="alert">' . $_SESSION['alert_message'] . '</div>';
     }
-    elseif($_SESSION['redirect_type'] = 'warning') {
-      $message = '<div class="alert alert-warning" role="alert">' . $_SESSION['redirect_message'] . '</div>';
+    else if($_SESSION['alert_type'] = 'warning') {
+      $message = '<div class="alert alert-warning" role="alert">' . $_SESSION['alert_message'] . '</div>';
     }
-    elseif($_SESSION['redirect_type'] = 'fail') {
-      $message = '<div class="alert alert-danger" role="alert">' . $_SESSION['redirect_message'] . '</div>';
+    else if($_SESSION['alert_type'] = 'fail') {
+      $message = '<div class="alert alert-danger" role="alert">' . $_SESSION['alert_message'] . '</div>';
     }
   }
+  else {
+    $message = '';
+  }
   return $message;
+}
+
+function clearAlertMessage() {
+  unset($_SESSION['alert_type']);
+  unset($_SESSION['alert_message']);
 }
 ?>
