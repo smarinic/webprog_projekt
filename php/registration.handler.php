687d67576b1b83ac10abac432e5515e243314514
smarinic <34188840+smarinic@users.noreply.github.com>
2022-03-18 12:03:44 +0100

    [task] add input validation on backend for POST

--- php/registration.handler.php
+++ php/registration.handler.php
@@ -1,8 +1,8 @@
 <?php
 
-include('dbconnection.php');
-include('functions.php');
-include('alert.message.handler.php');
+require_once(APP_ROOT . 'php/dbconnection.php');
+require_once(APP_ROOT . 'php/functions.php');
+require_once(APP_ROOT . 'php/alert.message.handler.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Backend za kreiranje korisnika u bazi
@@ -14,25 +14,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     else {
         if($_POST['password'] != $_POST['password_confirm']) {
-            require_once('alert.message.handler.php');
+            require_once(APP_ROOT . 'php/alert.message.handler.php');
             createAlertMessage('fail', 'Unosi lozinke nisu isti!');
             redirectPage('register.php');
         }
-        else {
-            $conn = createConnection();
-            $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, password, role_id) VALUES (?, ?, ?, ?, 3)");
-            $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
-            $firstname = clean_input($_POST["first_name"]);
-            $lastname = clean_input($_POST["last_name"]);
-            $email = clean_input($_POST["email"]);
-            $password = password_hash(clean_input($_POST["password"]), PASSWORD_DEFAULT);
-            $stmt->execute();
-        
-            $conn->close();
-        
-            createAlertMessage('success', 'Uspješna registracija! Možete se prijaviti u aplikaciju.');
-            redirectPage('../login.php');
+        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            createAlertMessage('fail', 'E-mail adresa nije ispravna!');
+            redirectPage('register.php');
         }
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST['first_name'])) {
+           createAlertMessage('fail', 'U imenu smiju biti samo slova i razmak!');
+           redirectPage('register.php');
+        }
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST['last_name'])) {
+           createAlertMessage('fail', 'U prezimenu smiju biti samo slova i razmak!');
+           redirectPage('register.php');
+        }
+        
+        $conn = createConnection();
+        $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, password, role_id) VALUES (?, ?, ?, ?, 3)");
+        $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
+        $firstname = clean_input($_POST["first_name"]);
+        $lastname = clean_input($_POST["last_name"]);
+        $email = clean_input($_POST["email"]);
+        $password = password_hash(clean_input($_POST["password"]), PASSWORD_DEFAULT);
+        $stmt->execute();
+    
+        $conn->close();
+    
+        createAlertMessage('success', 'Uspješna registracija! Možete se prijaviti u aplikaciju.');
+        redirectPage('../login.php');
     }
 }
 ?>
