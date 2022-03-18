68f416397eaac63f0f7438fd2b2f8ccbd63ee534
smarinic <34188840+smarinic@users.noreply.github.com>
2022-03-18 17:49:47 +0100

    [task] include alert code in globals.php

--- globals.php
+++ globals.php
@@ -4,4 +4,6 @@ define('APP_ROOT', __DIR__);
 
 require_once(APP_ROOT . '/php/session.php');
 require_once(APP_ROOT . '/php/functions.php');
+require_once(APP_ROOT . '/php/alert.message.handler.php');
+
 ?>
