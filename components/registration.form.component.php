a17240b46f661c706a17e7be8e1997e7965f8e83
smarinic <34188840+smarinic@users.noreply.github.com>
2022-03-23 12:52:03 +0100

    [fix] correct label attribute for

--- components/registration.form.component.php
+++ components/registration.form.component.php
@@ -13,17 +13,17 @@
       </div>
 
       <div class="form-outline mb-4">
-        <label class="form-label" for="form3Example3cg">Email</label>
+        <label class="form-label" for="emailInput">Email</label>
         <input type="email" id="emailInput" name="email" class="form-control form-control-lg" required>
       </div>
 
       <div class="form-outline mb-4">
-        <label class="form-label" for="form3Example4cg">Lozinka</label>
+        <label class="form-label" for="passwordInput">Lozinka</label>
         <input type="password" id="passwordInput" name="password" class="form-control form-control-lg" required>
       </div>
 
       <div class="form-outline mb-4">
-        <label class="form-label" for="form3Example4cg">Ponovi lozinku</label>
+        <label class="form-label" for="passwordConfirmInput">Ponovi lozinku</label>
         <input type="password" id="passwordConfirmInput" name="password_confirm" class="form-control form-control-lg" required>
       </div>
 
