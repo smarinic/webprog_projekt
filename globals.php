07b6487313d10e5b210045793f1964b4d065fc79
smarinic <34188840+smarinic@users.noreply.github.com>
2022-03-26 17:36:59 +0100

    [task] add fake json constant for testing

--- globals.php
+++ globals.php
@@ -2,6 +2,10 @@
 
 define('APP_ROOT', __DIR__);
 
+// true za koristenje fiksnih JSON-a u /test_data
+// za koristenje API-ja prebaci na false
+define('USE_FAKE_JSON_DATA', true);
+
 require_once(APP_ROOT . '/php/session.php');
 require_once(APP_ROOT . '/php/functions.php');
 require_once(APP_ROOT . '/php/alert.message.handler.php');
